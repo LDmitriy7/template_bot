8228360042e6204c0b0daf1488f44f85b72bff48
levch <levchenko.d.a1998@gmail.com>
2022-05-13 06:09:03 +0300

    .

--- handlers/for_admins/settings.py
+++ handlers/for_admins/settings.py
@@ -19,25 +19,7 @@ async def restart(msg: types.Message):
     async def _exit():
         dp.stop_polling()
         await dp.wait_closed()
-        await asyncio.sleep(5)
+        await asyncio.sleep(10)
         asyncio.get_event_loop().stop()
 
     asyncio.create_task(_exit())
-
-# @dp.message_handler(commands=commands.RESTART, user_id=config.admins.owner_id)
-# async def restart(msg: types.Message):
-#     _config = documents.Config.object() or documents.Config().save()
-#
-#     if _config.restart_date and (datetime.now() - _config.restart_date) < timedelta(seconds=10):
-#         await msg.answer('...')
-#         return
-#
-#     await msg.answer('Остановка бота...')
-#
-#     dp.stop_polling()
-#     await dp.wait_closed()
-#
-#     _config.restart_date = datetime.now()
-#     _config.save()
-#
-#     asyncio.get_event_loop().stop()
