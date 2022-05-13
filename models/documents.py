32ad68ae2bcc2151ddbf0c5fbcdec0aaca2fff31
levch <levchenko.d.a1998@gmail.com>
2022-05-13 06:04:53 +0300

    .

--- models/documents.py
+++ models/documents.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from datetime import datetime
+
 import mongoengine as me
 from aiogram_utils.mongoengine import Document
 
@@ -8,3 +10,4 @@ class Config(Document):
     admins_ids: list[int] = me.ListField(me.IntField())
     log_file: str = me.StringField()
     log_level: int = me.IntField()
+    restart_date: datetime = me.DateTimeField()
