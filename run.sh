9b06e4907e6b81cb4bb7206034c3c56497010a1f
levch <levchenko.d.a1998@gmail.com>
2022-05-14 05:04:47 +0300

    .

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
-python3 -m pip -q install poetry
+python3 -m pip -q install poetry pyyaml
 
-poetry -q install && poetry shell
 poetry export -o requirements.txt --without-hashes
 
 python3 on_startup.py
