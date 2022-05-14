b7974eb2db9eb024062ea359ed94c19ab85d3168
levch <levchenko.d.a1998@gmail.com>
2022-05-14 07:23:52 +0300

    .

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
-sudo apt-get -q install -q python3-pip docker-ce docker-ce-cli containerd.io docker-compose-plugin
-python3 -m pip -q install poetry pyyaml
+python3 -m pip -q install pyyaml
 
 poetry export -o requirements.txt --without-hashes
 python3 on_startup.py
