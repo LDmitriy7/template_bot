c59dec60a39d6fed5d56eacea5d25ddf146f6670
Dmitriy <levchenko.d.a1998@gmail.com>
2022-05-14 12:44:18 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-python3 -m pip -q install pyyaml
+python -m pip -q install pyyaml
 
 poetry export -o requirements.txt --without-hashes
-python3 on_startup.py
+python on_startup.py
 
 docker compose build
 docker compose up -d
